d4ddf2b7a64d7f432c7b32e66bc7c58ef5223483
Thays Omine <thaysomine@gmail.com>
2022-02-16 02:53:08 -0300

    Função para renderizar os quizzes da API adicionada

---
+++ script.js
@@ -0,0 +1,21 @@
+let quizData = [];
+// requisição para buscar todos os quizzes
+const promisse = axios.get("https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes");
+promisse.then(renderQuizzes);
+
+// função para renderizar os quizzes retirados da api
+function renderQuizzes(reply) {
+    quizData = reply.data;
+    console.log(quizData);
+    const quizList = document.querySelector(".all-quizzes");
+    console.log(quizList);
+
+    for (let i=0; i< quizData.length; i++) {
+        quizList.innerHTML +=`
+        <div class="quiz-main">
+            <img src="${quizData[i].image}" alt="modelo">
+            <div class="description">${quizData[i].title}</div>
+        </div>
+        `;
+    }
+}
