ff678ee2165553c675165a7db2f8201c6f48858f
Mariana Ito <marianaitopinto@gmail.com>
2022-02-16 11:30:49 -0300

    validando infos basicas na criacao do quizz

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ function renderQuizzes(reply) {
     const quizList = document.querySelector(".all-quizzes");
     console.log(quizList);
 
-    for (let i=0; i< quizData.length; i++) {
-        quizList.innerHTML +=`
+    for (let i = 0; i < quizData.length; i++) {
+        quizList.innerHTML += `
         <div class="quiz-main">
             <img src="${quizData[i].image}" alt="modelo">
             <div class="description">${quizData[i].title}</div>
@@ -21,10 +21,31 @@ function renderQuizzes(reply) {
 }
 
 
-//função para quando o usuário clicar pra criar quizz//
+//função para quando o usuário clicar pra criar quizz
 function createQuizz() {
     const hide = document.querySelector('.main-content');
     hide.classList.add('hiden');
     const show = document.querySelector('screen1-creating-quizz');
     show.classList.remove('hiden');
+}
+
+//função p salvar as informações básicas da criação do quizz
+
+function saveBasicInfoQuizz() {
+    const title = document.querySelector(".title-quizz").value;
+    const urlImg = document.querySelector(".image-url-quizz").value;
+    const qtQuestion = document.querySelector(".qt-questions-quizz").value;
+    const levels = document.querySelector(".level-quizz").value;
+
+    if ((title < 20) || (title > 65) || (qtQuestion < 4) || (levels < 3) || (validateURL(urlImg) === false)) {
+        alert("Preencha os dados corretamente!");
+    } else {
+        alert("ok");
+    }
+}
+
+// função p validar url (acho q vamos usar mais vezes)
+
+function validateURL(url) {
+    return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(url);
 }
